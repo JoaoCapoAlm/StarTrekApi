126708f9431318c0449d433a2539f8c97387a8fc
João Pedro <jpcapoani98@gmail.com>
2024-03-06 17:11:22 -0300

    Criado base de PlaceController e PlaceService

--- Domain/ViewModel/PlaceVM.cs
+++ Domain/ViewModel/PlaceVM.cs
@@ -2,7 +2,7 @@
 {
     public class PlaceVM
     {
-        public int ID { get; set; }
+        public short ID { get; set; }
         public string Name { get; set; }
         public byte QuadrantId { get; set; }
         public PlaceTypeVM PlaceType { get; set; }
