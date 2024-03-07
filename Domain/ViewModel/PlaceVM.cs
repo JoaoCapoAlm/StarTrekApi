477ddfe0e3849be7c5c66f02b58f0f58748a43bb
João Pedro <jpcapoani98@gmail.com>
2024-03-07 13:41:02 -0300

    Fix resource and add QuadrantVM

--- Domain/ViewModel/PlaceVM.cs
+++ Domain/ViewModel/PlaceVM.cs
@@ -4,7 +4,7 @@
     {
         public short ID { get; set; }
         public string Name { get; set; }
-        public byte QuadrantId { get; set; }
+        public QuadrantVM Quadrant { get; set; }
         public PlaceTypeVM PlaceType { get; set; }
     }
 }
