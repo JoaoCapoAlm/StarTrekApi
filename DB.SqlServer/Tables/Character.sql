23716d955394bc1ce696bfd571cc980e4ef9bdde
João Pedro <jpcapoani98@gmail.com>
2024-03-12 16:46:35 -0300

    Add characterclassification table

--- DB.SqlServer/Tables/Character.sql
+++ DB.SqlServer/Tables/Character.sql
@@ -5,6 +5,8 @@
     [DateBirth] DATE NULL, 
     [DateDeath] DATE NULL, 
     [SpeciesId] SMALLINT NOT NULL, 
+    [ClassificationId] INT NOT NULL, 
     CONSTRAINT [PK_Character] PRIMARY KEY ([CharacterId]),
-    CONSTRAINT [FK_Character_Species] FOREIGN KEY ([SpeciesId]) REFERENCES [dbo].[Species] ([SpeciesId])
+    CONSTRAINT [FK_Character_Species] FOREIGN KEY ([SpeciesId]) REFERENCES [dbo].[Species] ([SpeciesId]),
+    CONSTRAINT [FK_Character_Classification] FOREIGN KEY ([ClassificationId]) REFERENCES [dbo].[CharacterClassification] ([CharacterClassificationId])
 )
